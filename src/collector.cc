5176ed392752816c9991c413e3fb3017e165eab2
Tomasz Posluszny <tefposluszny@gmail.com>
2021-02-23 01:29:01 +0100

    Provide mongo connection details in config file

--- src/collector.cc
+++ src/collector.cc
@@ -10,39 +10,50 @@
 #include "websocket/poloniex_websocket_client.hpp"
 #include "consumer/mongo_ticker_consumer.hpp"
 #include "db/mongo_client.hpp"
+#include "utils/config.hpp"
 
 #include <iostream>
 #include <thread>
 
 int main(int argc, char* argv[]) {
-    (void)argc;
-    (void)argv;
+    if (argc < 2) {
+      std::cerr << "Please provide configuration file" << std::endl;
+      return -1;
+    }
+    std::string config_path(argv[1]);
+    auto config_json = cryptobot::GetConfigJson(config_path);
+    // TODO: take credentials from parameter store
+    std::string pass("DRt99xd4o7PMfygqotE8");
+    MongoClient* mongo_client =
+            MongoClient::GetInstance()->CreatePool("mongodb://" + config_json["user"].get<std::string>() + ":" + pass
+            + "@" + config_json["host"].get<std::string>() + ":" + config_json["port"].get<std::string>()
+            + "/?authSource=" + config_json["authSource"].get<std::string>());
+    std::cout << "Created Mongo client pool" << std::endl;
+    pass.clear();
+
+    MongoTickerConsumer mongo_consumer(mongo_client, "findata", "BtcUsdtTicker_v2");
+    // TODO: add Binance US for USDT-USD rates
+    BinanceWebsocketClient binance_client(&mongo_consumer);
+    // No USDT on Bitstamp
+    //BitstampWebsocketClient bitstamp_client(&mongo_consumer);
+    KrakenWebsocketClient kraken_client(&mongo_consumer);
+    // No USDT on Coinbase
+    //CoinbaseWebsocketClient coinbase_client(&mongo_consumer);
+    //PoloniexWebsocketClient poloniex_client(&mongo_consumer);
+    //BitbayWebsocketClient bitbay_client(&mongo_consumer);
+    //HuobiGlobalWebsocketClient huobi_global_client(&mongo_consumer);
+    // OkexWebsocketClient huobi_global_client(&mongo_consumer);
+    // BybitWebsocketClient bybit_client(&mongo_consumer);
+    //FtxWebsocketClient ftx_client(&mongo_consumer);
+    std::list<WebsocketClient*> websocket_client_list = {&binance_client,
+            &kraken_client//,
+            //&poloniex_client,
+            //&bitbay_client,
+            //&huobi_global_client,
+            //&ftx_client
+    };
 
     try {
-        // TODO: take credentials from parameter store ?
-        MongoClient* mongo_client =
-                MongoClient::GetInstance()->CreatePool("mongodb://app:DRt99xd4o7PMfygqotE8@18.132.211.87:28888/?authSource=findata");
-        MongoTickerConsumer mongo_consumer(mongo_client, "findata", "BtcUsdtTicker_v2");
-        // TODO: add Binance US for USDT-USD rates
-        BinanceWebsocketClient binance_client(&mongo_consumer);
-        // No USDT on Bitstamp
-        //BitstampWebsocketClient bitstamp_client(&mongo_consumer);
-        KrakenWebsocketClient kraken_client(&mongo_consumer);
-        // No USDT on Coinbase
-        //CoinbaseWebsocketClient coinbase_client(&mongo_consumer);
-        //PoloniexWebsocketClient poloniex_client(&mongo_consumer);
-        //BitbayWebsocketClient bitbay_client(&mongo_consumer);
-        //HuobiGlobalWebsocketClient huobi_global_client(&mongo_consumer);
-        // OkexWebsocketClient huobi_global_client(&mongo_consumer);
-        // BybitWebsocketClient bybit_client(&mongo_consumer);
-        //FtxWebsocketClient ftx_client(&mongo_consumer);
-        std::list<WebsocketClient*> websocket_client_list = {&binance_client,
-                &kraken_client//,
-                //&poloniex_client,
-                //&bitbay_client,
-                //&huobi_global_client,
-                //&ftx_client
-                };
         std::for_each(websocket_client_list.begin(), websocket_client_list.end(), [](WebsocketClient* tc) { tc->start(); });
         // Just wait for now
         std::this_thread::sleep_until(std::chrono::time_point<std::chrono::system_clock>::max());
