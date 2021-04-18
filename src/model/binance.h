1c3209d7e432bee29c80c7825a1cca094f3269e5
Tomasz Posluszny <tefposluszny@gmail.com>
2021-04-18 13:07:20 +0200

    Remove binance.h

--- src/model/binance.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern const std::unordered_map<std::string, SymbolId> BINANCE_ASSET_MAP = {
-  {"ADA", SymbolId::ADA},
-  {"BTC", SymbolId::BTC},
-  {"ETH", SymbolId::ETH},
-  {"EOS", SymbolId::EOS},
-  {"USDT", SymbolId::USDT}
-};
